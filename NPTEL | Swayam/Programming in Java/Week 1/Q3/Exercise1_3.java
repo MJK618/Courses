5b911ad1b29c82268302a6e2c4a64ca6285ea554
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-02-04 21:04:30 +0530

    Create Exercise1_3.java

---
+++ NPTEL | Swayam/Programming in Java/Week 1/Q3/Exercise1_3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Exercise1_3 {
+       public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int n=sc.nextInt();
+	  int sum=0;
+    
+    //Use for or while loop do the operation.
+    
+    
+    
+    
+     }
+}
