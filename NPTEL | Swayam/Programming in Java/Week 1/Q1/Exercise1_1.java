0081993e09b1e65599c2f79b4f36c68687d372dc
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-02-04 20:53:48 +0530

    Update Exercise1_1.java

--- NPTEL | Swayam/Programming in Java/Week 1/Q1/Exercise1_1.java
+++ NPTEL | Swayam/Programming in Java/Week 1/Q1/Exercise1_1.java
@@ -6,16 +6,6 @@ Scanner s = new Scanner(System.in);
        double perimeter;
        double area;
        
-            //Calculate the perimeter 
-            //Calculate the area
-            if (radius <= 0) {
-
-                System.out.println(" please enter non zero positive number");
-            } else {
-                area = Math.PI * radius * radius;
-                System.out.println(area);
-                perimeter = 2 * Math.PI * radius;
-                System.out.print(perimeter);
-            }       
+           //Write solution Here
     }
 }
