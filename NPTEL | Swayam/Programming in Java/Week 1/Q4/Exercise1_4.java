60f3ed7f6a16ac914d187c9fd7dc923fc14dcca6
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-02-04 21:23:26 +0530

    Create Exercise1_4.java

---
+++ NPTEL | Swayam/Programming in Java/Week 1/Q4/Exercise1_4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Exercise1_4 {
+    public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int n=sc.nextInt();
+           int result=0;
+           //Use while loop check the number is Armstrong or not.
+//store the output(1 or 0) in result variable.
+
+
+
+ }
+}
