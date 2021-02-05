28bda94a355939fec53d5ea99b8b1dea0429a1f9
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-02-05 22:53:49 +0530

    Create Question22.java

---
+++ NPTEL | Swayam/Programming in Java/Week 2/A2/Question22.java
@@ -0,0 +1,26 @@
+// This is the class named Printer
+class Printer { 
+    // This are the methods in class Printer
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+    public void print(String s) { 
+		System.out.println(s); 
+    } 
+} 
+
+public class Question22{ 
+    public static void main(String[] args) {    
+
+// Create an object of class Printer
+Printer pr = new Printer();
+
+// Call 'print()' methods for desired output
+pr.print("Hi! I am class STUDENT");
+pr.print();
+
+
+
+
+     } 
+}
