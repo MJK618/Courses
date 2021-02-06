13341777614d432b6c678e17fec97c5d7c26a986
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-02-06 22:20:55 +0530

    Create Question215.java

---
+++ NPTEL | Swayam/Programming in Java/Week 2/A5/Question215.java
@@ -0,0 +1,18 @@
+public class Question215{ 
+    public static void main(String[] args) { 
+    
+    
+    //Declare variable with name 'nptel', 'space' and 'java' and proper datatype.
+String nptel = "NPTEL";
+String space = " ";
+String java = "JAVA";
+
+//Initialize the variables with proper input
+
+
+
+
+
+     System.out.print(nptel+space+java);
+   }
+}
